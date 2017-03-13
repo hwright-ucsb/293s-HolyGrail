06a5a668f87225726262d810f02f60c1c5e0d657
Yantsey Tsai <yantseytsai@gmail.com>
2017-03-12 17:20:28 -0700

    calculate true ratings - still need to factor in attributes

--- leafly_reviews/calculate_true_rating.py
+++ leafly_reviews/calculate_true_rating.py
@@ -1,11 +1,38 @@
 import json, re
 
-def calcRating(review):
-	pass
+strain_rating = {}
+
+def calcRating(strain, star_ratings, attr):
+	if star_ratings:
+		avg_star = sum(star_ratings)/float(len(star_ratings))
+		strain_rating[strain] = {'avg': avg_star}
+	else:
+		strain_rating[strain] = {'avg': '-1'}
+
+
+
+
 
 data = json.load(open('strains_reviews-2.json'))
-print len(data)
-for i in range(len(data)):
-	cur = data[i]
-	rev = cur['reviews']
-	print rev['description']
+# print len(data)
+for strain_name in data:
+	obj = data[strain_name]
+
+	info = obj['info']
+	rev = obj['reviews']
+
+	attr_from_src = {} # unused for now
+	attr = {}
+	for i in info:
+		if i['source'] == 'LEAFLY': # seems like leafly is the only one to have useful attributes
+			attr_from_src[i['source']] = i['attributes'] # unused for now
+			attr = i['attributes']
+
+	star_ratings = []
+	for r in rev:
+		star = float(r['stars'])
+		star_ratings.append(star)
+
+	calcRating(strain_name, star_ratings, attr)
+
+print strain_rating
