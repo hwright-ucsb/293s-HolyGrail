ad8e38a4ab3b751fd950edbf9d0216e28ad713ae
Nikolas Chaconas <nikolas_chaconas@umail.ucsb.edu>
2017-03-12 20:14:30 -0700

    stopword and stemming in querystring

--- lucene/QueryString.java
+++ lucene/QueryString.java
@@ -9,110 +9,129 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.*;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import org.apache.lucene.util.Version;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import java.io.StringReader;
+import java.io.Reader;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.tartarus.snowball.ext.PorterStemmer;
+import org.apache.lucene.search.similarities.*;
+import java.util.HashMap;
+import org.apache.lucene.search.*;
+import org.apache.lucene.index.Term;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Scanner;
 
 public class QueryString {
-    public static String stemWords(String textFile) {
-        CharArraySet stopWords = EnglishAnalyzer.getDefaultStopSet();
-        StandardTokenizer tokenStream = new StandardTokenizer();
-        Reader targetReader = new StringReader(textFile.trim());
-        tokenStream.setReader(targetReader);
+	public static String removeStopWordsAndStem(String textFile) {
+		CharArraySet stopWords = EnglishAnalyzer.getDefaultStopSet();
+		StandardTokenizer tokenStream = new StandardTokenizer();
+		Reader targetReader = new StringReader(textFile.trim());
+		tokenStream.setReader(targetReader);
 
-        StringBuilder sb = new StringBuilder();
-        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
-        try{
-          tokenStream.reset();
-        }catch(IOException e){
-          e.printStackTrace();
-        }
-        PorterStemmer stemmer = new PorterStemmer();
-        
-        boolean tok = false;
+		StopFilter filter = new StopFilter(tokenStream, stopWords);
 
-        try{
-            tok = tokenStream.incrementToken();
-        }catch(IOException e){
-          e.printStackTrace();
-        }
+		StringBuilder sb = new StringBuilder();
+		CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+		try{
+		  filter.reset();
+		}catch(IOException e){
+		  e.printStackTrace();
+		}
+		PorterStemmer stemmer = new PorterStemmer();
+		
+		boolean tok = false;
 
-        while (tok) {
-            String term = charTermAttribute.toString();
-            
-            stemmer.setCurrent(term);
-            stemmer.stem();
-            String current = stemmer.getCurrent();
+		try{
+			tok = filter.incrementToken();
+		}catch(IOException e){
+		  e.printStackTrace();
+		}
 
-            sb.append(current + " ");
+		while (tok) {
+			String term = charTermAttribute.toString();
+			
+			stemmer.setCurrent(term);
+			stemmer.stem();
+			String current = stemmer.getCurrent();
 
-            
-            try {
-                tok = tokenStream.incrementToken();
-            } catch(IOException e){
-                e.printStackTrace();
-            }
+			sb.append(current + " ");
 
-        }
-        return sb.toString();
-    }
+			
+			try {
+				tok = filter.incrementToken();
+			} catch(IOException e){
+				e.printStackTrace();
+			}
 
-    public static void main(String[] args) throws IOException, ParseException {
-        int NUM_FILES = args.length > 1 ? Integer.parseInt(args[1]) : 100;
+		}
+		return sb.toString();
+	}
 
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-        FSDirectory index = FSDirectory.open(Paths.get("./Index" + NUM_FILES + ".lucene"));
+	public static void main(String[] args) throws IOException, ParseException {
+		int NUM_FILES = args.length > 1 ? Integer.parseInt(args[1]) : 100;
 
-        // 2. query
-        String querystr = args.length > 0 ? args[0] : "mexican";
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		FSDirectory index = FSDirectory.open(Paths.get("./Index" + NUM_FILES + ".lucene"));
 
-        // the first arg specifies the default field to use
-        // when no field is explicitly specified in the query.
-        Scanner sc = new Scanner(System.in);
-        // System.out.println("\nWhich field would you like to query? (Searches title by default.)");
-        // System.out.println("\tEnter 1 for DOC ID");
-        // System.out.println("\tEnter 2 for BODY");
-        // System.out.println("\tEnter anything else for TITLE");
+		// 2. query
+		String querystr = args.length > 0 ? args[0] : "mexican";
 
-        // int fieldCode = sc.nextInt();
-        int fieldCode = 1;
-        // sc.nextLine();
-        Query q;
-        IndexReader reader = null;
+		// the first arg specifies the default field to use
+		// when no field is explicitly specified in the query.
+		Scanner sc = new Scanner(System.in);
+		// System.out.println("\nWhich field would you like to query? (Searches title by default.)");
+		// System.out.println("\tEnter 1 for DOC ID");
+		// System.out.println("\tEnter 2 for BODY");
+		// System.out.println("\tEnter anything else for TITLE");
 
-        
-        while(!querystr.equals("exit")) {
-            System.out.println("Please Enter a query String!");
-            querystr = sc.nextLine();
+		// int fieldCode = sc.nextInt();
+		int fieldCode = 1;
+		// sc.nextLine();
+		Query q;
+		IndexReader reader = null;
 
-            if(fieldCode==1){
-                q = new QueryParser("both", analyzer).parse(querystr);
-            }
-            else if(fieldCode==2){
-                q = new QueryParser("body", analyzer).parse(querystr);
-            }
-            else{
-                q = new QueryParser("title",analyzer).parse(querystr);
-            }
+		
+		while(!querystr.equals("exit")) {
+			System.out.println("Please Enter a query String!");
+			querystr = sc.nextLine();
 
-                    // 3. search
-            int hitsPerPage = 10;
-            reader = DirectoryReader.open(index);
-            IndexSearcher searcher = new IndexSearcher(reader);
-            TopDocs docs = searcher.search(q, hitsPerPage);
-            ScoreDoc[] hits = docs.scoreDocs;
+			if(fieldCode==1){
+				q = new QueryParser("both", analyzer).parse(querystr);
+			}
+			else if(fieldCode==2){
+				q = new QueryParser("body", analyzer).parse(querystr);
+			}
+			else{
+				q = new QueryParser("title",analyzer).parse(querystr);
+			}
 
-            // 4. display results
-            System.out.println("Found " + hits.length + " hits.");
-            for(int i=0;i<hits.length;++i) {
-                int docId = hits[i].doc;
-                Document d = searcher.doc(docId);
-                System.out.println((i + 1) + ". " + "\t" + d.get("title") + "\n" + d.get("body") + "\n");
-            }
-        }
+					// 3. search
+			int hitsPerPage = 10;
+			reader = DirectoryReader.open(index);
+			IndexSearcher searcher = new IndexSearcher(reader);
+			TopDocs docs = searcher.search(q, hitsPerPage);
+			ScoreDoc[] hits = docs.scoreDocs;
 
-        // reader can only be closed when there
-        // is no need to access the documents any more.
-        reader.close();
-    }
+			// 4. display results
+			System.out.println("Found " + hits.length + " hits.");
+			for(int i=0;i<hits.length;++i) {
+				int docId = hits[i].doc;
+				Document d = searcher.doc(docId);
+				System.out.println((i + 1) + ". " + "\t" + d.get("title") + "\n" + d.get("body") + "\n");
+			}
+		}
+
+		// reader can only be closed when there
+		// is no need to access the documents any more.
+		reader.close();
+	}
 }
